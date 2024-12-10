70e8c6cac36a825b02d8ef5814dfa86e26a21153
Abdullah Al Fahad <abdullah.al.fahad@outlook.com>
2024-12-11 01:43:45 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Start Apache in the background
-apache2-foreground &
+# Start Laravel's built-in PHP server (use the background option &)
+php artisan serve --host=0.0.0.0 &
 
-# Run npm run dev after Apache starts
-npm run dev
+# Wait a little bit to make sure the server starts up
+sleep 5
+
+# Run npm run dev for the front-end
+npm run dev &
+
+# Keep the container running
+tail -f /dev/null
